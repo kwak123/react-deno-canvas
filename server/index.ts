07dc81a07b4db7bc93d2f6af52eb54f0ac5bf865
kwak123 <kwak123@gmail.com>
2020-03-27 17:56:18 -0400

    get a taste of typing

--- server/index.ts
+++ server/index.ts
@@ -1,8 +1,13 @@
-import { serve } from 'https://deno.land/std@v0.36.0/http/server.ts';
+import {
+  serve,
+  ServerRequest,
+} from 'https://deno.land/std@v0.36.0/http/server.ts';
+
 const s = serve({ port: 3000 });
 
 (async function() {
-  for await (const req of s) {
+  for await (const req of s as ServerRequest) {
+    console.log(req.method);
     req.respond({ body: 'Hello World\n' });
   }
 })();
