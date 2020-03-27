6463eefd7c4ec0121183c008fea4e81ca93e5ae6
kwak123 <kwak123@gmail.com>
2020-03-27 19:10:28 -0400

    update to latest deno

--- server/deps.ts
+++ server/deps.ts
@@ -1,7 +1,9 @@
-export { serve } from 'https://deno.land/std@v0.36.0/http/server.ts';
+export { serve } from 'https://deno.land/std@v0.37.1/http/server.ts';
 export {
   acceptWebSocket,
   isWebSocketCloseEvent,
   isWebSocketPingEvent,
   WebSocket,
-} from 'https://deno.land/std@v0.36.0/ws/mod.ts';
+} from 'https://deno.land/std@v0.37.1/ws/mod.ts';
+
+export { Application } from 'https://deno.land/x/oak/mod.ts';
