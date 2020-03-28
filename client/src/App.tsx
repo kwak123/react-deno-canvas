5852563391a4cf685365b879caf44f5edd4fa7b8
kwak123 <kwak123@gmail.com>
2020-03-28 00:22:50 -0400

    add ws deps, get ws working, add react and redux

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,27 @@
 import * as React from "react"
+import { w3cwebsocket as Socket } from "websocket"
 import Main from "./components/Main"
 
-class App extends React.Component {
-  render() {
-    return <Main />
-  }
+const App = () => {
+  let client: Socket
+
+  React.useEffect(() => {
+    client = new Socket("ws://127.0.0.1:8000/api/set-socket")
+    // client = new Socket("ws://demos.kaazing.com/echo")
+    client.onopen = () => {
+      console.log("Opening socket")
+    }
+    client.onmessage = (event) => {
+      console.log(event)
+    }
+  }, [])
+
+  return (
+    <>
+      <Main />
+      <button onClick={() => client.send("Hello")}>Send messsage</button>
+    </>
+  )
 }
 
 export default App
