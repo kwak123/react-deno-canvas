0747788ef6b44379fa1b480de2ca2a3a5829942a
kwak123 <kwak123@gmail.com>
2020-03-28 12:39:30 -0400

    get a line drawing on canvas, but lines are connecting

--- client/src/components/canvas/Canvas.tsx
+++ client/src/components/canvas/Canvas.tsx
@@ -1,28 +1,64 @@
-import React from "react"
+import React, { useState, useRef, MouseEvent } from "react"
 
+/* Adapted from https://stackoverflow.com/a/8398189 */
 const Canvas = () => {
-  const whiteboard = React.useRef<HTMLCanvasElement>(null)
+  const whiteboardRef = useRef<HTMLCanvasElement>(null)
+  const [shouldDraw, setShouldDraw] = useState(false)
+  const [isDot, setIsDot] = useState(false)
+  const [lastX, setLastX] = useState(0)
+  const [lastY, setLastY] = useState(0)
+  const [currX, setCurrX] = useState(0)
+  const [currY, setCurrY] = useState(0)
 
-  React.useEffect(() => {
-    const ctx = whiteboard.current.getContext("2d")
+  const draw = (ctx: CanvasRenderingContext2D) => {
     ctx.beginPath()
-    ctx.moveTo(100, 100)
-    ctx.fill()
-  })
+    ctx.moveTo(lastX, lastY)
+    ctx.lineTo(currX, currY)
+    ctx.strokeStyle = "black"
+    ctx.lineWidth = 2
+    ctx.stroke()
+    ctx.closePath()
+  }
 
-  const onMouseDown = () => {
-    /* Start drawing */
+  const onMouseDown = (event: MouseEvent) => {
+    const { current: whiteboard } = whiteboardRef
+    const ctx = whiteboard.getContext("2d")
+    setLastX(currX)
+    setLastY(currY)
+    setCurrX(event.clientX - whiteboard.offsetLeft)
+    setCurrY(event.clientY - whiteboard.offsetTop)
+    setShouldDraw(true)
+    setIsDot(true)
+
+    if (isDot) {
+      ctx.beginPath()
+      ctx.fillStyle = "black"
+      ctx.fillRect(currX, currY, 2, 2)
+      ctx.closePath()
+      setIsDot(false)
+    }
   }
+
   const onMouseUp = () => {
-    /* Stop drawing */
+    setShouldDraw(false)
+    setLastX(currX)
+    setLastY(currY)
   }
-  const onMouseMove = () => {
-    /* Draw */
+
+  const onMouseMove = (event: MouseEvent) => {
+    const { current: whiteboard } = whiteboardRef
+    if (shouldDraw) {
+      setLastX(currX)
+      setLastY(currY)
+      setCurrX(event.clientX - whiteboard.offsetLeft)
+      setCurrY(event.clientY - whiteboard.offsetTop)
+      draw(whiteboard.getContext("2d"))
+    }
   }
 
   return (
     <canvas
-      ref={whiteboard}
+      ref={whiteboardRef}
       id="whiteboard"
       width="600"
       height="600"
