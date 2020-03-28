a6aeeecfb912f783e377a3ebd51f5fbdcef5b1ca
kwak123 <kwak123@gmail.com>
2020-03-28 00:16:12 -0400

    remove oak stuff, add file size headers

--- server/index.ts
+++ server/index.ts
@@ -14,47 +14,6 @@ import { addSocket, pingSocket } from './socket/socket.ts';
 import { walkSync } from 'https://deno.land/std@v0.37.1/fs/walk.ts';
 const port = Deno.args[0] || '8080';
 
-// const router = new Router();
-
-// router.get('/api/set-socket', async (context, next) => {
-//   console.debug('Requesting socket');
-//   await addSocket(context.request.serverRequest);
-//   console.debug('Socket set');
-// });
-
-// router.post('/api/test/:uuid', async context => {
-//   if (context.params.uuid) {
-//     console.log(context.params.uuid);
-//     await pingSocket(context.params.uuid);
-//   }
-// });
-
-// const app = new Application();
-
-// app.use(async ({ request }, next) => {
-//   await next();
-//   console.log(`Received ${request.method} request to ${request.path}`);
-// });
-
-// app.use(router.routes());
-// app.use(router.allowedMethods());
-
-// // Server static files
-// app.use(async ctx => {
-//   try {
-//     await send(ctx, ctx.request.path, {
-//       root: `${Deno.cwd()}/../dist`,
-//       index: 'index.html',
-//     });
-//   } catch (e) {
-//     // No favicon yet
-//     console.error(e);
-//   }
-// });
-
-// console.log(`App listening on ${port}`);
-// await app.listen({ port: Number(port) });
-
 const tryToServeFile = async (fileName: string) => {
   const distDirectory = `${Deno.cwd()}/../dist`;
   const filePath = `${distDirectory}${fileName}`;
@@ -63,14 +22,18 @@ const tryToServeFile = async (fileName: string) => {
       Deno.open(filePath),
       Deno.stat(filePath),
     ]);
-    // const headers = new Headers();
-    // headers.set('content-length', fileInfo.size.toString());
-    // headers.set('content-type', 'text/plain; charset=utf-8');
+
+    const headers = new Headers();
+    headers.set('content-length', fileInfo.size.toString());
+
+    if (fileName.includes('.css')) {
+      headers.set('content-type', 'text/css');
+    }
 
     const res = {
       status: 200,
       body: file,
-      // headers,
+      headers,
     };
     return res;
   } catch (e) {
@@ -81,21 +44,6 @@ const tryToServeFile = async (fileName: string) => {
   }
 };
 
-const connected = new Set<WebSocket>();
-const lines: string[] = [];
-
-const initClient = async (socket: WebSocket) => {
-  connected.add(socket);
-
-  for await (const event of socket.receive()) {
-    if (typeof event === 'string') {
-      lines.push(event);
-      connected.forEach(socket => socket.send(JSON.stringify(lines)));
-    }
-  }
-};
-
-console.log(`websocket server is running on :${port}`);
 for await (const req of serve(`:${port}`)) {
   const { headers, conn } = req;
   try {
@@ -113,54 +61,10 @@ for await (const req of serve(`:${port}`)) {
       }
     }
     if (req.url === '/api/set-socket') {
-      const ws = await acceptWebSocket({
-        conn,
-        headers,
-        bufReader: req.r,
-        bufWriter: req.w,
-      });
-      initClient(ws);
+      addSocket(req);
     }
   } catch (e) {
     const res = { status: 404 };
     req.respond(res);
   }
 }
-
-// .then(
-//   async (sock: WebSocket): Promise<void> => {
-//     console.log('socket connected!');
-//     const it = sock.receive();
-//     while (true) {
-//       try {
-//         const { done, value } = await it.next();
-//         if (done) {
-//           break;
-//         }
-//         const ev = value;
-//         if (typeof ev === 'string') {
-//           // text message
-//           console.log('ws:Text', ev);
-//           await sock.send(ev);
-//         } else if (ev instanceof Uint8Array) {
-//           // binary message
-//           console.log('ws:Binary', ev);
-//         } else if (isWebSocketPingEvent(ev)) {
-//           const [, body] = ev;
-//           // ping
-//           console.log('ws:Ping', body);
-//         } else if (isWebSocketCloseEvent(ev)) {
-//           // close
-//           const { code, reason } = ev;
-//           console.log('ws:Close', code, reason);
-//         }
-//       } catch (e) {
-//         console.error(`failed to receive frame: ${e}`);
-//         await sock.close(1000).catch(console.error);
-//       }
-//     }
-//   },
-// )
-// .catch((err: Error): void => {
-//   console.error(`failed to accept websocket: ${err}`);
-// });
--- server/socket/socket.ts
+++ server/socket/socket.ts
@@ -19,45 +19,14 @@ export const addSocket = async (request: ServerRequest) => {
     bufWriter: request.w,
   });
 
-  const it = socket.receive();
+  const socketId = v4.generate();
+  sockets.set(socketId, socket);
 
-  await it.next();
-  // while (true) {
-  //   try {
-  //     const { done, value } = await it.next();
-  //     if (done) {
-  //       break;
-  //     }
-  //     const ev = value;
-  //     if (typeof ev === 'string') {
-  //       // text message
-  //       console.log('ws:Text', ev);
-  //       await socket.send(ev);
-  //     } else if (ev instanceof Uint8Array) {
-  //       // binary message
-  //       console.log('ws:Binary', ev);
-  //     } else if (isWebSocketPingEvent(ev)) {
-  //       const [, body] = ev;
-  //       // ping
-  //       console.log('ws:Ping', body);
-  //     } else if (isWebSocketCloseEvent(ev)) {
-  //       // close
-  //       const { code, reason } = ev;
-  //       console.log('ws:Close', code, reason);
-  //     }
-  //   } catch (e) {
-  //     console.error(`failed to receive frame: ${e}`);
-  //     await socket.close(1000).catch(console.error);
-  //   }
-  // }
-
-  // const uuid = v4.generate();
-  // sockets.set(uuid, socket);
-  // console.log('Set socket with uuid ' + uuid);
-  // await socket.send('Hi');
-  setTimeout(() => {
-    console.log(socket.isClosed);
-  }, 1000);
+  for await (const event of socket.receive()) {
+    if (typeof event === 'string') {
+      sockets.forEach(socket => socket.send(event));
+    }
+  }
 };
 
 export const pingSocket = async (uuid: string) => {
@@ -69,51 +38,3 @@ export const pingAllSockets = async () => {
     await socket.send('Ping!');
   });
 };
-
-// console.log(`websocket server is running on :${port}`);
-// for await (const req of serve(`:${port}`)) {
-//   const { headers, conn } = req;
-//   acceptWebSocket({
-//     conn,
-//     headers,
-//     bufReader: req.r,
-//     bufWriter: req.w,
-//   })
-//     .then(
-//       async (sock: WebSocket): Promise<void> => {
-//         console.log('socket connected!');
-//         const it = sock.receive();
-//         while (true) {
-//           try {
-//             const { done, value } = await it.next();
-//             if (done) {
-//               break;
-//             }
-//             const ev = value;
-//             if (typeof ev === 'string') {
-//               // text message
-//               console.log('ws:Text', ev);
-//               await sock.send(ev);
-//             } else if (ev instanceof Uint8Array) {
-//               // binary message
-//               console.log('ws:Binary', ev);
-//             } else if (isWebSocketPingEvent(ev)) {
-//               const [, body] = ev;
-//               // ping
-//               console.log('ws:Ping', body);
-//             } else if (isWebSocketCloseEvent(ev)) {
-//               // close
-//               const { code, reason } = ev;
-//               console.log('ws:Close', code, reason);
-//             }
-//           } catch (e) {
-//             console.error(`failed to receive frame: ${e}`);
-//             await sock.close(1000).catch(console.error);
-//           }
-//         }
-//       },
-//     )
-//     .catch((err: Error): void => {
-//       console.error(`failed to accept websocket: ${err}`);
-//     });
-// }
