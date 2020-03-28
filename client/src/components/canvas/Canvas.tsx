0db60653a485f19f9156d1265df28af7ed607536
kwak123 <kwak123@gmail.com>
2020-03-28 11:23:19 -0400

    get a rectangle to show on canvas!

--- client/src/components/canvas/Canvas.tsx
+++ client/src/components/canvas/Canvas.tsx
@@ -1,7 +1,16 @@
 import React from "react"
 
 const Canvas = () => {
-  return <canvas></canvas>
+  const whiteboard = React.useRef<HTMLCanvasElement>(null)
+
+  React.useEffect(() => {
+    const ctx = whiteboard.current.getContext("2d")
+    ctx.fillRect(10, 10, 50, 50)
+  })
+
+  return (
+    <canvas ref={whiteboard} id="whiteboard" width="600" height="600"></canvas>
+  )
 }
 
 export default Canvas
