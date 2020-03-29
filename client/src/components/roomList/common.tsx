e22e3c0a621b2aabc955f4636ec03ba6284ecea1
kwak123 <kwak123@gmail.com>
2020-03-29 12:58:01 -0400

    add hover effect to card

--- client/src/components/roomList/common.tsx
+++ client/src/components/roomList/common.tsx
@@ -1,10 +1,17 @@
 import styled from "styled-components"
 
 export const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
+  position: relative;
+  top: 0;
   width: 300px;
   height: 420px;
   background-color: white;
+  border-radius: 8px;
+  box-shadow: 0 2px 6px 2px #757575;
+  transition: top 0.2s, box-shadow 0.2s;
+
+  &:hover {
+    top: -4px;
+    box-shadow 0 4px 12px 4px #757575;
+  }
 `
