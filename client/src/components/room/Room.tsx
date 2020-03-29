35afd546c4ab22be76d9fcc26a2eb987f317996c
kwak123 <kwak123@gmail.com>
2020-03-29 15:18:21 -0400

    get room details from component

--- client/src/components/room/Room.tsx
+++ client/src/components/room/Room.tsx
@@ -1,6 +1,10 @@
-import React, { useEffect } from "react"
+import React, { useEffect, useState } from "react"
 import { useParams } from "react-router"
+import { useDispatch } from "react-redux"
 import services from "../../services"
+import { Room } from "../../services/rooms"
+
+import { setStrokes } from "../../store/canvas/actions"
 
 import Canvas from "../canvas/Canvas"
 
@@ -9,18 +13,25 @@ interface RoomQueryParams {
 }
 
 const Room = () => {
+  const dispatch = useDispatch()
   const params = useParams<RoomQueryParams>()
   const roomId = params.roomId
+  const [room, setRoom] = useState<Room>(null)
 
   useEffect(() => {
     services.socketService.initializeSocket(roomId)
+    services.roomsService
+      .getRoom(roomId)
+      .then((roomFromApi) => setRoom(roomFromApi))
     return function clean() {
       services.socketService.closeSocket()
+      dispatch(setStrokes([]))
     }
   }, [roomId])
 
   return (
     <div>
+      <h2>{room?.title || "Untitled"}</h2>
       <Canvas />
     </div>
   )
--- client/src/services/rooms.ts
+++ client/src/services/rooms.ts
@@ -3,6 +3,7 @@ import { CanvasStroke } from "../store/canvas/reducers"
 
 enum RoomsEndpoint {
   getAllRooms = "/api/rooms",
+  getRoom = "/api/room",
 }
 
 export interface Room {
@@ -19,6 +20,17 @@ export class RoomsHelper {
     try {
       const roomNamesResponse = await axios.get(RoomsEndpoint.getAllRooms)
       return roomNamesResponse.data as RoomResponse
+    } catch (e) {
+      console.error(e)
+      return null
+    }
+  }
+
+  async getRoom(roomId: string) {
+    try {
+      const roomEndpoint = `${RoomsEndpoint.getRoom}/${roomId}`
+      const roomDetailsResponse = await axios.get(roomEndpoint)
+      return roomDetailsResponse.data as Room
     } catch (e) {
       console.error(e)
       return null
