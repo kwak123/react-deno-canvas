90a458216f21f003b3c764324572360b4d471e95
kwak123 <kwak123@gmail.com>
2020-03-29 13:56:10 -0400

    style createRoom button

--- client/src/components/roomList/AddRoomCard.tsx
+++ client/src/components/roomList/AddRoomCard.tsx
@@ -2,12 +2,47 @@ import React from "react"
 import styled from "styled-components"
 
 import { Container } from "./common"
+import { COLORS } from "../styling"
 
-const AddCardSymbol = styled.div``
+const AddCardContainer = styled.div`
+  background-color: ${COLORS.BLUE_OCEAN};
+  display: flex;
+  align-item: center;
+  justify-content: center;
+  width: 100%;
+  height: 100%;
+  border-radius: 6px;
+`
+
+const ButtonContainer = styled.div`
+  display: flex;
+  align-items: center;
+  color: ${COLORS.WHITE_CREAM};
+`
+
+const AddSignSpan = styled.span`
+  font-size: 24px;
+  margin-right: 8px;
+`
+
+const CreateRoom = styled.button`
+  color: ${COLORS.WHITE_CREAM};
+  background-color: transparent;
+  padding: 0;
+  border: none;
+  font-size: 24px;
+  text-decoration: none;
+  height: fit-content;
+`
 
 const AddRoomCard = () => (
   <Container>
-    <AddCardSymbol>+</AddCardSymbol>
+    <AddCardContainer>
+      <ButtonContainer>
+        <AddSignSpan>+</AddSignSpan>
+        <CreateRoom>Create Room</CreateRoom>
+      </ButtonContainer>
+    </AddCardContainer>
   </Container>
 )
 
